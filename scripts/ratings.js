c1cd0d23087e485ea14affb9471b09a1219d3e59
LizLoftus <ejoloftus@outlook.com>
2021-05-04 23:36:17 -0400

    practicing what i've already learned

---
+++ scripts/ratings.js
@@ -0,0 +1,14 @@
+//These are functions that can pretty much go wherever on the site.
+//calculate received ratings
+
+let ratingsForm = document.querySelector("ratings-form");
+
+ratingsForm.addEventListener("submit", function(event) {
+    console.log("in event listener");
+    calculateRating();
+    event.preventDefault();
+  }, false);
+
+function calculateRating(){
+    console.log("event listener works." + ratingsForm);
+}
