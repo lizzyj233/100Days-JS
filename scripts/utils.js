9ac1c7a837312f8845e2ff0e44ea160e78792d4b
LizLoftus <ejoloftus@outlook.com>
2021-06-03 22:51:36 -0400

    split switch to own html

---
+++ scripts/utils.js
@@ -0,0 +1,5 @@
+//write footer to html
+(function (){
+    document.getElementById("footer").innerHTML = `<p>connect with me on twitter <a href="https://twitter.com/lizzydeb233">@lizzydeb233</a></p>
+    <p>find my stuff on <a href="https://github.com/lizzyj233">Github</a></p>  `;
+})();
