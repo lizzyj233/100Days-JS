92960ffe156f55c8825ff3de57e0a7f7613e58fb
Liz Loftus <65726009+lizzydeb233@users.noreply.github.com>
2021-06-04 13:20:43 -0400

    arrow functions and closures

--- scripts/JS-Funcs.js
+++ scripts/JS-Funcs.js
@@ -9,3 +9,49 @@ function printAll(){
 }
 printAll(1, 2, 3, 4, 5);
 printAll(10, 20);
+
+//Immediately Invoked Function Expressions - IIFE -- this will be invoked right away!
+(function (){
+    console.log("We're in an IIFE!");
+})();
+
+//use an IIFE to assign value to variable
+let message = (function (){
+    return "Hi!";
+})();
+console.log(message);
+
+//closures prevent variables defined within fns from going out of scope once the fn is executed
+//use a variable as invoking a function
+function setupCounter (num){
+    return function Counter(){
+        return num++;
+    }
+}
+let count = setupCounter(0);
+console.log(count()); //0
+console.log(count()); //1
+let count2 = setupCounter(10);
+console.log(count2()); //10
+
+//Arrow Functions = a more compact way of writing a function expression
+//arrown funcs in other languages are known as "lambda" expressions
+
+//regular anonymous function
+let greetings = function(){
+    return "I'm a regular anonymous fn...";
+}
+let regAnFn = greetings();
+console.log(regAnFn);
+console.log(greetings());
+
+//as an arrow function
+let greetings2 = () => "I'm an arrow function!"; //no need to return; string is implicitly assigned to greetings2 variable
+let arrFn = greetings2();
+console.log(arrFn);
+console.log(greetings2());
+
+//an arrow fn taking a parameter
+let greetings3 = myName => "Hello " + myName;
+let myMessage = greetings3("Liz");
+console.log(myMessage);
